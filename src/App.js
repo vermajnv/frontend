336b3f84f3615e6d8b952d48cc942c3879dcbe21
nayan <nayanrahul.jnv@gmail.com>
2024-05-08 12:16:31 +0530

    Fixes for production build

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import React, { Suspense} from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Users from "./user/pages/Users";
-import NewPlace from "./places/pages/NewPlace";
+
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
-import UserPlaces from "./places/pages/UserPlaces";
-import UpdatePlace from "./places/pages/UpdatePlace";
-import Authenticate from "./user/pages/Authenticate";
 import { AuthContext } from "./shared/context/auth-context";
 import PrivateRoutes from "./shared/utils/PrivateRoutes";
 import { useAuth } from "./shared/hooks/auth-hook";
+import LoadingSpinner from "./shared/components/UIElements/Loading/LoadingSpinner";
+
+const Users = React.lazy(() => import('./user/pages/Users'))
+const NewPlace = React.lazy(() => import('./places/pages/NewPlace'))
+const UserPlaces = React.lazy(() => import('./places/pages/UserPlaces'))
+const UpdatePlace = React.lazy(() => import('./places/pages/UpdatePlace'))
+const Authenticate = React.lazy(() => import('./user/pages/Authenticate'))
 
 function App() {
   const {login, logout, token, userId} = useAuth();
@@ -25,21 +29,23 @@ function App() {
       <Router>
         <main>
           <MainNavigation></MainNavigation>
-          <Routes>
-            <Route path="/" element={<Users></Users>} exact></Route>
-            <Route path="/:userId/places" element={<UserPlaces />}></Route>
-            <Route
-              path="/places"
-              element={<PrivateRoutes isAuthenticated={token} />}
-            >
-              <Route path="new" element={<NewPlace></NewPlace>}></Route>
+          <Suspense fallback={<div className="center"><LoadingSpinner asOverlay></LoadingSpinner></div>}>
+            <Routes>
+              <Route path="/" element={<Users></Users>} exact></Route>
+              <Route path="/:userId/places" element={<UserPlaces />}></Route>
               <Route
-                path=":placeId"
-                element={<UpdatePlace></UpdatePlace>}
-              ></Route>
-            </Route>
-            <Route path="/auth" element={<Authenticate></Authenticate>}></Route>
-          </Routes>
+                path="/places"
+                element={<PrivateRoutes isAuthenticated={token} />}
+              >
+                <Route path="new" element={<NewPlace></NewPlace>}></Route>
+                <Route
+                  path=":placeId"
+                  element={<UpdatePlace></UpdatePlace>}
+                ></Route>
+              </Route>
+              <Route path="/auth" element={<Authenticate></Authenticate>}></Route>
+            </Routes>
+          </Suspense>
         </main>
       </Router>
     </AuthContext.Provider>
--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -35,7 +35,7 @@ const MainNavigation = (props) => {
                 <span></span>
             </button>
             <h1 className='main-navigation__title'>
-                <Link to="/">Frontend</Link>
+                <Link to="/">Places</Link>
             </h1>
             <nav className='main-navigation__header-nav'>
                 <NavLinks></NavLinks>
